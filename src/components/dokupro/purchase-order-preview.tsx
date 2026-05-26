c3383e4734b66c760d3a1426794d44db86d53e46
Z User <z@container>
2026-05-26 17:33:23 +0000

    468cef5a-8be5-47f5-8dbe-2433b9b2b640

--- src/components/dokupro/purchase-order-preview.tsx
+++ src/components/dokupro/purchase-order-preview.tsx
@@ -168,7 +168,7 @@ export function PurchaseOrderPreview({ data }: PurchaseOrderPreviewProps) {
       {/* Signatures - 3 columns */}
       <div className="print-sig-grid grid grid-cols-3 gap-3 mt-3 text-[10px] text-center print:text-[8px] print:mt-[1mm]">
         <div>
-          <p className="font-semibold mb-4 print:mb-4 text-black">Disetujui Oleh</p>
+          <p className="font-semibold mb-4 print:mb-4 text-black">Toko</p>
           <div className="mx-auto w-3/5 pb-0.5" style={{ borderBottom: '1px solid #000000' }} />
         </div>
         <div>
@@ -176,7 +176,7 @@ export function PurchaseOrderPreview({ data }: PurchaseOrderPreviewProps) {
           <div className="mx-auto w-3/5 pb-0.5" style={{ borderBottom: '1px solid #000000' }} />
         </div>
         <div>
-          <p className="font-semibold mb-4 print:mb-4 text-black">Toko</p>
+          <p className="font-semibold mb-4 print:mb-4 text-black">Disetujui Oleh</p>
           <div className="mx-auto w-3/5 pb-0.5" style={{ borderBottom: '1px solid #000000' }} />
         </div>
       </div>
