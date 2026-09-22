3107a8317be9730e3418392841c8dfa366989375
Z User <z@container>
2026-09-22 15:36:45 +0000

    chore: bump PWA sw v114 / app version 2026-09-22-v49

--- src/components/service-worker-registration.tsx
+++ src/components/service-worker-registration.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react'
 
 // App version - bump this when deploying new content to force users to get fresh version
-const APP_VERSION = '2026-09-21-v48'
+const APP_VERSION = '2026-09-22-v49'
 const IS_DEV = process.env.NODE_ENV !== 'production'
 
 export function ServiceWorkerRegistration() {
